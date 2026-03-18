f142af9cada0f9f79e9ab7db5849baca0477aaf8
aabuble <giacky597@gmail.com>
2026-03-18 11:30:05 +0100

    Commit

--- src/MainServer.java
+++ src/MainServer.java
@@ -3,32 +3,56 @@ import java.net.*;
 
 public class MainServer {
 
+    //colore messaggi SERVER
+    public static final String BLU = "\u001B[34m";
+    //colore messaggi CLIENT
+    public static final String VERDE = "\033[1;31m";
+    //colore RESET
+    public static final String RESET = "\033[0m";
+
     public static void main(String[] args) {
-        System.out.println("Inizio SERVER!!!");
+        System.out.println(BLU + "Inizio SERVER!!!" + RESET);
 
         try {
             //scelta primitiva socket e operazione di 'bind' esplicita perché ho scelto la porta
             DatagramSocket dSocket = new DatagramSocket(3000);
+            System.out.println(BLU + "Apertura della porta per ricevere!!!" + RESET);
 
-            //decido in quale parte della RAM mettere le mie istruzioni
-            byte[] bufferIn = new byte[256];
-            //leggo tutto il bufferIn con il 'length'
-            DatagramPacket inPacket = new DatagramPacket(bufferIn, bufferIn.length);
+            while (true) {
+                //decido in quale parte della RAM mettere le mie istruzioni
+                byte[] bufferIn = new byte[256];
+                //leggo tutto il bufferIn con il 'length'
+                DatagramPacket inPacket = new DatagramPacket(bufferIn, bufferIn.length);
 
-            dSocket.receive(inPacket); //METODO BLOCCANTE
-            System.out.println("Ricezione messaggio effettuata: " + inPacket);
+                dSocket.receive(inPacket); //METODO BLOCCANTE
+                System.out.println("Ricezione messaggio effettuata: " + inPacket);
 
-            //ECHO
-            //Deserializzazione (trasformo i byte in una stringa)
-            String message = new String(inPacket.getData(), 0, inPacket.getLength());
+                InetAddress clientAddress = inPacket.getAddress();
+                int port = inPacket.getPort();
 
-            InetAddress clientAddress = inPacket.getAddress();
-            int port = inPacket.getPort();
+                //ECHO
+                //Deserializzazione (trasformo i byte in una stringa)
+                String messageIn = new String(inPacket.getData(), 0, inPacket.getLength());
+                System.out.println(VERDE + "Messaggio ricevuto dal CLIENT" + clientAddress + ": " + port + "\n\t" + messageIn + RESET);
 
-            DatagramPacket outPacket = new DatagramPacket(message.getBytes(), message.length(), clientAddress, port);
-            dSocket.send(outPacket);
+                String messageOut = "Richiesta ricevuta!!!";
+                DatagramPacket outPacket = new DatagramPacket(messageOut.getBytes(), messageOut.length(), clientAddress, port);
 
-            //NO chiusura SERVER perché deve poter sempre ricevere 24/24
+                dSocket.send(outPacket);
+                System.out.println(BLU + "Messaggio spedito al CLIENT: " + messageOut + RESET);
+
+                //recuperiamo l'indirizzo del gruppo
+                InetAddress groupAddress = InetAddress.getByName("239.255.255.250");
+                int groupPort = 2500;
+
+                messageOut= "Benvenuti a tutti!";
+
+                outPacket = new DatagramPacket(messageOut.getBytes(), messageOut.length(), groupAddress, groupPort);
+
+                dSocket.send(outPacket);
+                System.out.println(BLU + "Messaggio spedito al gruppo: " + messageOut + RESET);
+                //NO chiusura SERVER perché deve poter sempre ricevere 24/24
+            }
 
         } catch (BindException e) {
             System.err.println("Porta già in uso!!!");
